2d08a60fe4f09897d2493d2967c8835c5d8f9bd4
David <emterdavid5@gmail.com>
2026-01-15 12:55:30 +0100

    Initial commit: Labarix landing page with Vue 3 and Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import LandingPage from './components/LandingPage.vue'
+</script>
+
+<template>
+  <LandingPage />
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+</style>
